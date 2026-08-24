b6b72db82812bb0f659fb25cd7e4fc2585032500
Graham Place <grahamgplace@gmail.com>
2026-08-24 10:14:54 -0700

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,34 @@
+'''
+aba -> true
+i = a, j = a 
+i = b, j = b 
+return True 
+
+abba
+i = a, j = a 
+i = b, j = b 
+
+- case-insensitive 
+- ignores all non-alphanumeric characters
+'''
+
+class Solution:
+    # Two pointers approach avoids addtl memory required
+    #  - O(n) time, O(1) space
+    # Compare reversed strings = O(n) space, O(n) time
+    def isPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s) - 1
+        while i <= j:
+            if not s[i].isalnum():
+                i += 1
+                continue
+            
+            if not s[j].isalnum():
+                j -= 1
+                continue
+
+            if s[i].lower() != s[j].lower(): return False
+            i += 1
+            j -= 1
+
+        return True
