61665915ef99aa57087a7f0abc61ed183f8a40b2
Graham Place <grahamgplace@gmail.com>
2026-08-25 09:43:13 -0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,32 @@
+'''
+FIFO: [1] [1, 2]
+MIN:  [1] [1, 1]
+'''
+
+
+from typing import Optional, List 
+
+
+class MinStack:
+
+    def __init__(self):
+        self._stack: List[int] = []
+        self._min_stack: List[int] = [] 
+        self.size: int = 0
+
+    def push(self, val: int) -> None:
+        self._stack.append(val)
+        min_at_val_insert = min(self._min_stack[-1], val) if self._min_stack else val
+        self._min_stack.append(min_at_val_insert)
+        self.size += 1
+
+    def pop(self) -> None:
+        self._stack.pop()
+        self._min_stack.pop()
+        self.size -= 1
+
+    def top(self) -> int:
+        return self._stack[-1]
+
+    def getMin(self) -> int:
+        return self._min_stack[-1]
