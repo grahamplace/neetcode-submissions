c8b23c76e0779e6f7d1a69844655fff3a2183824
Graham Place <grahamgplace@gmail.com>
2026-08-25 11:57:38 -0700

    Add: two-integer-sum-ii - submission-14

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-14.py
@@ -0,0 +1,66 @@
+'''
+Given numbers 
+- numbers
+- already sorted in non-decreasing (so duplicates allowed?)
+- e.g. [1,2,3,4], 
+
+
+OUTPUT
+- 1 INDEXED indices
+- of 2 elems that add up to target
+- AND index1 < index 2 (NOT equal, can't use self + self)
+
+CONSTRAINTS 
+- O(1) additional space.
+- There will always be exactly ONE valid solution
+- input has at least 2 elems, no empty checks needed
+- negative numbers ARE allowed
+
+IDEA
+- Sliding Window, start at min + max (0, last)
+- if sum of those is too big: no solution
+- if sum is too small: move the pointer that max the minimum change in sum 
+- converge on solution 
+- if pointers ever cross or ==, no solution (can't use same elem twice)
+- O(n)
+
+
+WALKTHROUGH
+t = 5 [-5, -3, 0, 2, 4, 6, 8]
+[-5L, -3, 0, 2, 4, 6, 8R] => 3
+'''
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
+
+        while left < right:
+            curr = numbers[left] + numbers[right]
+
+            if curr == target:
+                return [left + 1, right + 1]
+
+            move_left_new_sum = numbers[left + 1] + numbers[right]
+            move_right_new_sum = numbers[left] + numbers[right - 1]
+
+            # make the move that keeps the max sum (under target), so that we step thru all possible valid sums in decreasing order until we hit target
+
+            # if moving left exceeds target, we should never do that move
+            if move_left_new_sum > target:
+                right -= 1 
+
+            elif move_left_new_sum >= move_right_new_sum:
+                left += 1
+            else:
+                right -= 1
+        
+        # should never happen if we have valid inputs
+        assert False, "Unexpectedly reached end of twoSum!"
+        return [-1, -1]
+
+
+
+
+
+        
