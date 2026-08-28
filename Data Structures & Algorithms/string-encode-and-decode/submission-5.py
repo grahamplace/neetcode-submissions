257aa67f3eb998e5bc2697c1da6a7db3ff420d71
Graham Place <grahamgplace@gmail.com>
2026-08-28 15:47:25 -0700

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,35 @@
+class Solution:
+
+    DELIM = '#'
+
+    def encode(self, strs: List[str]) -> str:
+        output = ''
+        for s in strs: 
+            encoded = f'{self.DELIM}{len(s)}{self.DELIM}{s}'
+            output += encoded
+        
+        return output
+
+    def decode(self, s: str) -> List[str]:
+        output, i = [], 0
+
+        while i < len(s):
+            if s[i] == self.DELIM:
+                j = i + 1
+                count_str = ''
+                while j < len(s):
+                    if s[j] == self.DELIM:
+                        break
+                    else:
+                        count_str += s[j]
+                    j += 1
+
+                if count_str:
+                    i_len = int(count_str)
+                    output.append(s[j+1:j+i_len+1])
+                    i = j + i_len + 1
+            
+        return output
+
+            
+
