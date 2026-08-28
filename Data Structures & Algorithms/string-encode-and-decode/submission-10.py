4fb855768f26b1d4f8ec34326a9bcfdaa500cc54
Graham Place <grahamgplace@gmail.com>
2026-08-28 15:51:26 -0700

    Add: string-encode-and-decode - submission-10

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-10.py
@@ -0,0 +1,28 @@
+class Solution:
+    DELIM = "#"
+
+    def encode(self, strs: List[str]) -> str:
+        output = []
+        for s in strs:
+            encoded = f"{self.DELIM}{len(s)}{self.DELIM}{s}"
+            output.append(encoded)
+
+        return ''.join(output)
+
+    def decode(self, s: str) -> List[str]:
+        output, i = [], 0
+
+        while i < len(s):
+            if s[i] == self.DELIM:  # First delim starts the count string of the next string
+                j = i + 1
+                while j < len(s):
+                    if s[j] == self.DELIM:  # Next delim ends the count string
+                        break
+                    j += 1
+
+                word_len = int(s[i + 1:j])
+                word_end = j + word_len + 1
+                output.append(s[j + 1 : word_end])
+                i = word_end  # Continue on from the char following the end of this word
+
+        return output
