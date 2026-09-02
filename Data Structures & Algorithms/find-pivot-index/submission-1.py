1819269858152b4bdb48b1baf5d5dc9f72bb7a20
Graham Place <grahamgplace@gmail.com>
2026-09-02 11:53:40 -0700

    Add: find-pivot-index - submission-1

---
+++ Data Structures & Algorithms/find-pivot-index/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        prefix_sums = [0]
+        for n in nums:
+            prefix_sums.append(prefix_sums[-1] + n)
+        
+
+        postfix_sums = [0]
+        j = len(nums) - 1
+        while j >= 0:
+            postfix_sums.append(postfix_sums[-1] + nums[j])
+            j -= 1
+        
+        postfix_sums.reverse()
+        
+        for i in range(1, len(nums) + 1):
+            if prefix_sums[i] == postfix_sums[i - 1]:
+                return i - 1
+            
+        return -1
+        
