8385e00e6df7766bfebbf392d655e67713016278
Graham Place <grahamgplace@gmail.com>
2026-09-08 17:46:55 -0700

    Add: design-twitter-feed - submission-6

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-6.py
@@ -0,0 +1,54 @@
+from collections import OrderedDict
+from typing import Dict, Set
+from heapq import heappush, heappop
+
+class Twitter:
+    
+    MAX_SIZE = 10
+
+    def __init__(self):
+        self.tweets_by_users = {}
+        self.follows_for_user: Dict[int, set[int]] = {}
+        self.tweet_counter: int = 0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        if userId not in self.tweets_by_users:
+            self.tweets_by_users[userId] = [(self.tweet_counter, tweetId)]
+        else:
+            tweets = self.tweets_by_users[userId]
+            if len(tweets) == self.MAX_SIZE:
+                heappop(self.tweets_by_users[userId])
+
+            heappush(tweets, (self.tweet_counter, tweetId))
+        
+        self.tweet_counter += 1
+
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        all_tweets = []
+        for u in (list(self.follows_for_user.get(userId, [])) + [userId]):
+            all_tweets.extend(self.tweets_by_users.get(u, []))
+        
+        all_tweets.sort(key=lambda x: x[0], reverse=True)
+        return [t[1] for t in all_tweets[:10]]
+
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId == followeeId: return
+
+        if followerId not in self.follows_for_user:
+            self.follows_for_user[followerId] = {followeeId}
+        else: 
+            self.follows_for_user[followerId].add(followeeId)
+
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if (
+            followerId == followeeId or 
+            followerId not in self.follows_for_user or 
+            followeeId not in self.follows_for_user[followerId]
+        ): return
+        
+        self.follows_for_user[followerId].remove(followeeId)
+
+        
