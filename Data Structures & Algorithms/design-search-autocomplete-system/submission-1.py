c964cd0e839362be1bb036534b6d70fedc026cf0
Graham Place <grahamgplace@gmail.com>
2026-09-17 15:40:59 -0700

    Add: design-search-autocomplete-system - submission-1

---
+++ Data Structures & Algorithms/design-search-autocomplete-system/submission-1.py
@@ -0,0 +1,115 @@
+'''
+Users may input a sentence (at least one word and end with a special character '#').
+
+Given:
+sentences: ["i love you", "island", "iroman", "i love neetcode"]
+times: [5, 3, 2, 2] (same len as sentences)
+- sentences[i] is a previously typed sentence 
+- times[i] is the corresponding number of times the sentence was typed
+
+Output: 
+For each input character except '#':
+return the top 3 historical hot sentences that have the same prefix as the part of the sentence already typed
+
+Rules for "hot sentences":
+- The hot degree for a sentence is defined as the number of times a user typedtly the exac same sentence before.
+- The returned top 3 hot sentences should be sorted by hot degree (The first is the hottest one)
+    - If several sentences have the same hot degree, use ASCII-code order (smaller one appears first)
+- If less than 3 hot sentences exist, return as many as you can.
+- When the input is a special character, it means the sentence ends, and in this case, you need to return an empty list
+
+input(c) continues to build on itself
+intput("i") then input(" ") looks up "i "
+
+Example
+["i love you", "island", "iroman", "i love neetcode"] / [5, 3, 2, 2]
+input("i") => ["i love you", "island", "i love neetcode"] (top 3 hottest of all matches starting w "i")
+input(" ") => ["i love you", "i love neetcode"] (top 3 hottest best effort starting with "i ")
+
+IMPORTANT:
+- When user finishes input by sending "#", the sentence they just ended should be saved as a historical sentence in system. - - And the following input will be counted as a new search!
+'''
+
+from collections import defaultdict
+
+class TrieNode:
+    def __init__(self):
+        self.sentence_end_count: int = 0
+        self.children: dict[str, TrieNode] = {}
+
+class AutocompleteSystem:
+
+    SIZE = 3
+
+    def __init__(self, sentences: List[str], times: List[int]):
+        self.current_query = ""
+        self.sentences: dict[str, int] = defaultdict(int)
+        self.trie_root = TrieNode()
+
+        for sentence, t in zip(sentences, times):
+            self.sentences[sentence] += t
+            self._insert_sentence_trie(sentence, t)
+
+    def _insert_sentence_trie(self, sentence: str, times: int) -> None:
+        curr = self.trie_root
+        for c in sentence: 
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            
+            curr = curr.children[c]
+        
+        curr.sentence_end_count += times
+
+
+    def _return_sentences_below_node(self, prefix: str, node: TrieNode) -> set[str]:
+        results = set()
+        if node.sentence_end_count > 0:
+            results.add(prefix)
+        
+        for k, child in node.children.items():
+            results = results.union(self._return_sentences_below_node(prefix + k, child))
+        
+        return results
+
+
+    def _search_prefix(self, prefix: str) -> set[str]:
+        curr = self.trie_root
+        for c in prefix:
+            if c not in curr.children:
+                return set()
+            
+            curr = curr.children[c]
+        
+        # now we need ALL sentences below this node in the trie:
+        return self._return_sentences_below_node(prefix, curr)
+
+
+    def _run_query(self, query: str) -> list[str]:
+        # we can use Trie to get only the subset of sentences that match this prefix, rather than all N
+        matches = self._search_prefix(query)
+        #  If several sentences have the same hot degree, use ASCII-code order (smaller one appears first).
+        return sorted(matches, key=lambda x: (-self.sentences[x], x))[:self.SIZE]
+
+    def input(self, c: str) -> List[str]:
+        if c == '#':
+            self.sentences[self.current_query] += 1
+            self._insert_sentence_trie(self.current_query, 1)
+            self.current_query = ""
+            return []
+
+        self.current_query += c
+        return self._run_query(self.current_query)
+
+
+obj = AutocompleteSystem(["ab", "ab"], [2, 2])
+assert obj._run_query("a") == ["ab"]
+
+obj = AutocompleteSystem(["ab", "ab", "abc"], [2, 2, 10])
+assert obj._run_query("a") == ["abc", "ab"]
+        
+
+
+
+
+
+
